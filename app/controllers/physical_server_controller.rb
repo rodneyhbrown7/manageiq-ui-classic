55660387225cbdc789f7fcfc7d8537590627f377
Walter Alves <walter.arruda.alves@gmail.com>
2017-01-30 15:22:24 -0300

    Add physical server views
    Adding all physical servers view templates.
    Use Mixin Show methods
    Using show_list method from Mixin in physical server controller.
    Rename views Physical server's view folder
    Renaming physical servers view folders to 'physicalserver'
    Change physical server view path
    Change back the physical server view structure to physical_server.
    Remove specific show method in controller
    Using default show method and clen up physical server controller.

--- app/controllers/physical_server_controller.rb
+++ app/controllers/physical_server_controller.rb
@@ -10,9 +10,6 @@ class PhysicalServerController  < ApplicationController
   after_action :set_session_data
 
 
-  def self.model
-    ManageIQ::Providers::PhysicalServer
-  end
 
   def self.table_name
     @table_name ||= "physical_servers"
