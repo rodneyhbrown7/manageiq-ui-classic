efdbbc0a8309b31bd9af7058f913afd3d096152f
Rodney H. Brown <rbrown4@lenovo.com>
2017-02-02 08:53:15 -0500

    Updates to the UI classic to enable walter's ui
    Add "physical server to host" relationship in Physical server Summary
    Use Physical server's host in summary as a link to drill down from Physical server summary to Host summary.
    Add physical server controller
    Added Physical server controller with basic actions(show and show_list).
    Configure Physical server actions
    Configuring show and show_list actions in physical_server_controller.rb.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -219,6 +219,9 @@ module ApplicationHelper
       if controller == "ems_infra" && action == "show"
         return ems_infras_path
       end
+      if controller == "ems_physical_infra" && action == "show"
+        return ems_physical_infras_path
+      end      
       if controller == "ems_container" && action == "show"
         return ems_containers_path
       end
@@ -1472,6 +1475,8 @@ module ApplicationHelper
 
   def db_for_quadicon
     case @layout
+    when "ems_physical_infra"
+      :ems    
     when "ems_infra"
       :ems
     when "ems_cloud"
