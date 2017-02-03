b03d5ccc461ace310e59b4e5a82db770be0f9bee
Walter Alves <walter.arruda.alves@gmail.com>
2017-02-03 17:19:25 -0300

    Add sidebar to Physical server pages.
    Add a sidebar to physical server pages and add the structure to sidebar to ems physical infra pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1387,9 +1387,11 @@ module ApplicationHelper
              ems_middleware
              ems_network
              ems_storage
+             ems_physical_infra
              flavor
              floating_ip
              host
+             physical_server
              host_aggregate
              load_balancer
              middleware_datasource
